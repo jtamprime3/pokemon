ddc036ff6e1e647875de59b7fba1922a2843813e
jtamprime <jtamprime@gmail.com>
2024-06-09 15:29:27 -0400

    my first commit message

---
+++ pokemon.rb
@@ -0,0 +1,28 @@
+require 'io/console'
+def continue
+  print "Press any key to continue "
+  STDIN.getch
+  print "             \r"
+end
+
+puts 'Hello there! '
+continue
+puts 'Welcome to the world of pokemon!'
+continue
+puts 'Welcome my name is Oak!    '
+continue
+puts 'People call me the pokemon prof!'
+continue
+puts 'This world is inhabited by creatures called POKEMON!'
+continue
+puts 'For some people, pokemon are pets!'
+continue
+puts 'Others use them for fights!'
+continue
+puts 'Myself...I study POKEMON as a profession!'
+continue
+puts 'First, what is your name?'
+name = gets.chomp
+puts "Hello #{name}"
+continue
+
