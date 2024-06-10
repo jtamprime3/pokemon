e9e1da192bf2fea683c7715ad8660df574909a71
jtamprime <jtamprime@gmail.com>
2024-06-09 23:09:10 -0400

    Update pokemon.rb
    Complete code

--- pokemon.rb
+++ pokemon.rb
@@ -5,26 +5,50 @@ def continue
   print "             \r"
 end
 
-puts 'Hello there! '
+puts 'Oak: Hello there! '
 continue
-puts 'Welcome to the world of pokemon!'
+puts '     Welcome to the world of pokemon!'
 continue
-puts 'Welcome my name is Oak!    '
+puts '     Welcome my name is Oak!    '
 continue
-puts 'People call me the pokemon prof!'
+puts '     People call me the pokemon prof!'
 continue
-puts 'This world is inhabited by creatures called POKEMON!'
+puts '     This world is inhabited by creatures called POKEMON!'
 continue
-puts 'For some people, pokemon are pets!'
+puts '     For some people, pokemon are pets!'
 continue
-puts 'Others use them for fights!'
+puts '     Others use them for fights!'
 continue
-puts 'Myself...I study POKEMON as a profession!'
+puts '     Myself...I study POKEMON as a profession!'
 continue
-puts 'First, what is your name?'
+puts '     First, what is your name?'
+puts 'Type any name'
 name = gets.chomp
-puts "Hello #{name}"
+puts "Oak: Right! So your name is #{name}!"
 continue
-# puts "hello world"
-puts "testing"
+puts '     This is my grandson.'
+continue
+puts '     He has been your rival since you were a baby.'
+continue
+puts '     ...Erm, what is his name again?'
+puts 'Type any name'
+rival_name = gets.chomp
+puts "     That's right!            "
+continue
+puts "     I remember now!         "
+continue
+puts "     His name is #{rival_name}!              "
+continue
+puts "     #{name}!                  "
+continue
+puts "     Your very own POKEMON legend is about to unfold!"
+continue
+puts "     A world of dreams and adventures with POKEMON awaits!"
+continue
+puts "     Let's go!                    "
+continue
+
+
+
+
 
