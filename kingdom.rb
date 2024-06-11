51daf052c38421c7983e47f9626bea5b0aae64d1
jtamprime <jtamprime@gmail.com>
2024-06-10 22:40:44 -0400

    Update kingdom.rb
    complete kingdom hearts code

---
+++ kingdom.rb
@@ -0,0 +1,35 @@
+require 'io/console'
+def continue
+  print "Press any key to continue "
+  STDIN.getch
+  print "             \r"
+end
+
+puts "Sora: I've been having these weird thoughts lately."
+continue
+puts "       Like, is any of this for real, or not?"
+continue
+puts "King Mickey: So much to do, so little time..."
+continue
+puts "             Take your time. "
+continue
+puts "             Don't be afraid."
+continue
+puts "             The door is still shut."
+continue
+puts "             Now, step forward. Can you do it?"
+continue
+puts "Type walk to walk or run to run"
+input = gets.chomp
+until input == "walk" || input == "run"
+  puts "try again type either walk or run"
+  input = gets.chomp
+end
+if input == "walk"
+  puts "walking"
+end
+if input == "run"
+  puts "running"
+end
+continue
+puts "King Mickey: Power sleeps within you…"
