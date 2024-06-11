ba9d84f2cc7986a2aae32938a83e43910d1287b1
jtamprime3 <jtamprime@gmail.com>
2024-06-11 12:50:41 -0400

    Hopefully Last update

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -2,7 +2,7 @@ require 'io/console'
 def continue
   print "Press any key to continue "
   STDIN.getch
-  print "             \r"
+  print "             \r" # Clears the line after key press
 end
 
 puts "You are talking to deaf grandma"
@@ -17,8 +17,8 @@ until bye_counter == 3 do
   puts "HUH?! SPEAK UP, SONNY!" unless input == uppercase
   puts "NOT SINCE 19#{rand(30..58)}" if input == uppercase
   bye_counter += 1 if input == "BYE"
-  bye_counter = 0 if input =! "BYE"
-  puts "Grandma saids Bye" if bye_counter == 3
+  bye_counter = 0 if input != "BYE"
+  puts "Grandma says Bye" if bye_counter == 3
 end
 
  
