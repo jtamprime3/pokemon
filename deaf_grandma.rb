86994106c3f5ca61cbf7b2df893fbf337957ae7a
jtamprime <jtamprime@gmail.com>
2024-06-10 23:32:46 -0400

    Complete

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,1 +1,41 @@
-puts "hello world"
+# puts "hello world"
+require 'io/console'
+def continue
+  print "Press any key to continue "
+  STDIN.getch
+  print "             \r"
+end
+puts "Welcome to the deaf grandma program"
+continue
+puts "Say anything.                "
+
+#uppercase = ""
+# input = gets.chomp
+# bye_counter = 0
+# until input == uppercase && bye_counter < 3
+#   uppercase = input.upcase
+
+#   if input != uppercase
+#     puts "HUH?! SPEAK UP, SONNY!"
+#     input = gets.chomp
+#   else
+#     puts "NOT SINCE 19#{rand(30..58)}"
+#     input = gets.chomp
+#     bye_counter += 1
+#   end
+# end
+# puts "Bye Sonny" if bye_counter == 3
+
+
+bye_count = 0
+until bye_count == 3 do
+  speak = gets.chomp
+  im_yelling = speak == speak.upcase
+  said_bye = speak == 'BYE'
+
+  puts "HUH?! SPEAK UP, SONNY!" unless im_yelling
+  puts "NO NOT UNTIL #{rand(1930..1950)}" if im_yelling
+
+  bye_count = 0 unless said_bye
+  bye_count = bye_count + 1 if said_bye
+end
--- kingdom.rb
+++ kingdom.rb
@@ -26,7 +26,35 @@ until input == "walk" || input == "run"
   input = gets.chomp
 end
 
-puts "walking" if input == "walk"
-puts "running" if input == "run"
-continue
-puts "King Mickey: Power sleeps within you…"
+# puts "walking" if input == "walk"
+# puts "running" if input == "run"
+# continue
+# puts "King Mickey: Power sleeps within you…"
+
+# puts "Sora: I've been having these weird thoughts lately."
+# puts "      Like, is any of this for real, or not?"
+# puts "King Mickey: So much to do, so little time..."
+# puts "             Take your time. Don't be afraid."
+# puts "             The door is still shut."
+# puts "King Mickey: Now, step forward. Can you do it?"
+
+# puts "On-screen: type 'walk' to move."
+# puts "           [type 'RUN' to run.]"
+
+
+
+# input = ""
+# walked = ""
+# ran = ""
+
+# until walked == 'yes' && ran == 'yes' do
+#   input = gets.chomp
+
+#   walked = 'yes' if input == 'walk'
+#   ran = 'yes' if input == 'RUN'
+
+#   puts "hey I'm walking" if input == 'walk'
+#   puts "OMG I'm RUNNING" if input == 'RUN'
+# end
+
+# puts 'King Mickey: Power sleeps within you…'
